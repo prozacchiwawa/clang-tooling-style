aba14f50cd57091ccbff43f031544e7a84505369
arty <art.yerkes@gmail.com>
2015-05-21 18:53:20 +0000

    Add test file

---
+++ tooltest/testnewdelete.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <map>
+#include <memory>
+#include <vector>
+#include <utility>
+
+int *x;
+
+int main() {
+    x = new int();
+    delete x;
+}
